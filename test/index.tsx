496d9ad7f9dc1d955b5b7bc3a81888bfb58beb86
郑瑜琨 <zyk185387@alibaba-inc.com>
2019-05-30 00:57:42 +0800

    test webpack

---
+++ test/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'antd'
+import 'antd/dist/antd.less'
+
+ReactDOM.render(
+  <>
+  <h1>123</h1>
+  <Button>dddd</Button>
+  </>,
+  document.getElementById('app'),
+)
